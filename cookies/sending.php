e2bd6bbb613d9cfcabb02cc20c6f7596c57de6bf
kazaku lopez nathan <19k212@esisalama.org>
2022-01-24 09:13:40 +0100

    impression de base de cookies de la gestion de cookies avec php/ documentation commentaire de la fonciton setcookie

--- cookies/sending.php
+++ cookies/sending.php
@@ -15,7 +15,21 @@
             $sexe = $_POST['sexe'];
 
             // session ne fonctionne pas avec les données de la methode post directement
-            setcookie('name', $value = $name);
+            /*
+            * setcookie prend en paremettre :
+            * NAME du cookie
+            * VALUE du cookie qui sera accessible avec le nom
+            * EXPIRES_OR_OPTIONS : le temps qu'il mettra, fixer la vleur avec time()
+            * en y ajoutant le nombre en seconde ainsi 30 jours = time()*3600*24*30
+            * path (str): le chemin sur le serveur sur lequel le cookie sera disponible, si la valeur vaut '/' il sera partout
+            * doamin (str): sous-doamine
+            * secure (bool) : true le cookies ne sera envoyer que si la connexin est securisé
+            * httponly : true le cookie ne sera accessibleque par le protocol HTTP
+            * option : le tableau
+            * la valeur de retour de cette fonction est soit true soit false , true si le cookie fontionne et false dans le cas inversels
+            * les données de cookies sont accessible dans le tableau super global $_COOKIE
+            */
+            setcookie('name', $value = $name, time() * 3600);
             setcookie('sexe', $value = $sexe);
 
             echo 'your last name is :'.$_COOKIE['name'].'<br/>';
